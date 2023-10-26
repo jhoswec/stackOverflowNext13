5e5e8da2f593ecb3bff23cfca3fdb9abf6572121
Jhoswe Genner Castro Castañeda <jhoswe.castro@gmail.com>
2023-10-25 20:02:42 -0500

    homepage finished with all the components

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,50 @@
 import Filter from "@/components/shared/Filter";
-import HomeFilters from "@/components/shared/home/HomeFilters";
+import NoResult from "@/components/shared/NoResult";
+import HomeFilters from "@/components/home/HomeFilters";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sqlalchemy" },
+    ],
+    author: {
+      _id: "3",
+      name: "John Doe",
+      picture: "john-doe.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div?",
+    tags: [
+      { _id: "4", name: "css" },
+      { _id: "5", name: "html" },
+    ],
+    author: {
+      _id: "6",
+      name: "Jane Smith",
+      picture: "jane-smith.jpg",
+    },
+    upvotes: 5,
+    views: 50,
+    answers: [],
+    createdAt: new Date("2021-09-02T08:30:00.000Z"),
+  },
+];
+
+// Note that I've made changes to match the data types and structure of the QuestionProps interface.
 
 export default function Home() {
   return (
@@ -31,6 +72,33 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* looping through questions */}
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          // "No Results Found"}
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! aks a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,54 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - createdAt.getTime();
+
+  // Define time units in milliseconds
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const week = 7 * day;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  if (timeDifference < minute) {
+    const secondsAgo = Math.floor(timeDifference / 1000);
+    return `${secondsAgo} second${secondsAgo !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < hour) {
+    const minutesAgo = Math.floor(timeDifference / minute);
+    return `${minutesAgo} minute${minutesAgo !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < day) {
+    const hoursAgo = Math.floor(timeDifference / hour);
+    return `${hoursAgo} hour${hoursAgo !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < week) {
+    const daysAgo = Math.floor(timeDifference / day);
+    return `${daysAgo} day${daysAgo !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < month) {
+    const weeksAgo = Math.floor(timeDifference / week);
+    return `${weeksAgo} week${weeksAgo !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < year) {
+    const monthsAgo = Math.floor(timeDifference / month);
+    return `${monthsAgo} month${monthsAgo !== 1 ? "s" : ""} ago`;
+  } else {
+    const yearsAgo = Math.floor(timeDifference / year);
+    return `${yearsAgo} year${yearsAgo !== 1 ? "s" : ""} ago`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
